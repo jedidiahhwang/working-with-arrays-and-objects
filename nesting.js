d0a346062dbb67506920e7c57d3a393abd07fe57
Jedidiah Hwang <jedidiah.hwang@gmail.com>
2020-11-09 21:12:54 -0700

    Final submission

--- nesting.js
+++ nesting.js
@@ -55,13 +55,15 @@ var employees = [
 function employeeUpdater () {
   for (let i = 0; i < employees.length; i++) {
     if (employees[i].firstName === "Theo") {
-      delete employees[i];
+      employees.splice(i, 1);
     } else if (employees[i].firstName === "Lorie") {
-      employees[i].department === "HR";
+      employees[i].department = "HR";
     }
   }
   return employees;
 }
+
+console.log(employees);
 
 
 
@@ -80,6 +82,18 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
+
+function removeDuplicates (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        arr.splice(j, 1);
+        j--;
+      }
+    }
+  }
+  return arr;
+}
 
 
 
@@ -108,8 +122,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
@@ -151,6 +165,18 @@ var myCar = {
 
 //Code Here
 
+function recordCleaner () {
+  for (let key in myCar) {
+    if (typeof myCar[key] === "object") {
+      for (let prop in myCar[key]) {
+          myCar[key][prop].atFaultForAccident = false;
+      }
+    }
+  }
+}
+
+console.log(myCar);
+
 
 
 ////////// PROBLEM 5 //////////
@@ -169,5 +195,20 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 */
 
 //Code Here
+
+function looper () {
+  numsArr.forEach((element) => {
+    for (let i = 0; i < element.length; i++) {
+      if (element[i] % 2 === 0) {
+        element[i] = "even";
+      } else {
+        element[i] = "odd";
+      }
+    }
+  })
+  return numsArr;
+}
+
+console.log(numsArr);
 
 
